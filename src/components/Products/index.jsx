f6ff1d7e17965d95715909a6571625bde111ca31
Tran The Hai <haitt060699@gmail.com>
2020-08-01 00:05:24 +0700

    product-list commit

---
+++ src/components/Products/index.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./index.css";
+import formatCurrency from "../../util.js";
+
+class Products extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="products">
+          {this.props.products.map((product) => (
+            <li key={product.id}>
+              <div className="product">
+                <a href={"#" + product.id}>
+                  <img src={product.image} alt={product.title} />
+                  <p>{product.title}</p>
+                </a>
+                <div className="product-price">
+                  <div>{formatCurrency(product.price)}</div>
+                  <button className="button primary">Add to cart</button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Products;
